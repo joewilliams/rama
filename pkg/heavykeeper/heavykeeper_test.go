7c711f052c05e5a613238ccf2bdac8cdf0ab7b5d
Joe Williams <williams.joe@gmail.com>
2023-01-15 15:42:00 -0800

    heavykeeper tests

--- pkg/heavykeeper/heavykeeper_test.go
+++ pkg/heavykeeper/heavykeeper_test.go
@@ -10,57 +10,72 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewAndAdd(t *testing.T) {
+func TestSmall(t *testing.T) {
 	ips := []netip.Addr{
-		netip.MustParseAddr("192.168.1.1"),
-		netip.MustParseAddr("192.168.1.2"),
-		netip.MustParseAddr("192.168.1.3"),
-		netip.MustParseAddr("192.168.1.3"),
-		netip.MustParseAddr("192.168.1.3"),
-		netip.MustParseAddr("192.168.1.3"),
-		netip.MustParseAddr("192.168.1.3"),
-		netip.MustParseAddr("192.168.1.4"),
-		netip.MustParseAddr("192.168.1.4"),
-		netip.MustParseAddr("192.168.1.4"),
-		netip.MustParseAddr("192.168.1.4"),
-		netip.MustParseAddr("192.168.1.5"),
-		netip.MustParseAddr("192.168.1.5"),
-		netip.MustParseAddr("192.168.1.6"),
-		netip.MustParseAddr("192.168.1.6"),
-		netip.MustParseAddr("192.168.1.6"),
-		netip.MustParseAddr("192.168.1.6"),
-		netip.MustParseAddr("192.168.1.6"),
-		netip.MustParseAddr("192.168.1.6"),
-		netip.MustParseAddr("192.168.1.6"),
-		netip.MustParseAddr("192.168.1.7"),
-		netip.MustParseAddr("192.168.1.7"),
-		netip.MustParseAddr("192.168.1.7"),
+		netip.MustParseAddr("192.0.2.6"),
+		netip.MustParseAddr("192.0.2.1"),
+		netip.MustParseAddr("192.0.2.2"),
+		netip.MustParseAddr("192.0.2.3"),
+		netip.MustParseAddr("192.0.2.3"),
+		netip.MustParseAddr("192.0.2.3"),
+		netip.MustParseAddr("192.0.2.3"),
+		netip.MustParseAddr("192.0.2.3"),
+		netip.MustParseAddr("192.0.2.4"),
+		netip.MustParseAddr("192.0.2.4"),
+		netip.MustParseAddr("192.0.2.4"),
+		netip.MustParseAddr("192.0.2.4"),
+		netip.MustParseAddr("192.0.2.5"),
+		netip.MustParseAddr("192.0.2.5"),
+		netip.MustParseAddr("192.0.2.6"),
+		netip.MustParseAddr("192.0.2.6"),
+		netip.MustParseAddr("192.0.2.6"),
+		netip.MustParseAddr("192.0.2.6"),
+		netip.MustParseAddr("192.0.2.6"),
+		netip.MustParseAddr("192.0.2.6"),
+		netip.MustParseAddr("192.0.2.7"),
+		netip.MustParseAddr("192.0.2.7"),
+		netip.MustParseAddr("192.0.2.7"),
+		netip.MustParseAddr("192.0.2.6"),
 	}
 
-	topk := New(5, 10, 10, 0.99, 1234567812345678)
+	// this test is deterministic until we hit width*depth
+	topk := New(5, 10, 5, 0.9, 1234567812345678)
 
 	for _, ip := range ips {
 		topk.Add(ip)
 	}
 
-	get := topk.Get()
-
-	assert.Equal(t, uint64(7), get[netip.MustParseAddr("192.168.1.6")])
-	assert.Equal(t, uint64(5), get[netip.MustParseAddr("192.168.1.3")])
-	assert.Equal(t, uint64(4), get[netip.MustParseAddr("192.168.1.4")])
-	assert.Equal(t, uint64(3), get[netip.MustParseAddr("192.168.1.7")])
-	assert.Equal(t, uint64(2), get[netip.MustParseAddr("192.168.1.5")])
-
-	want := []netip.Addr{
-		netip.MustParseAddr("192.168.1.6"),
-		netip.MustParseAddr("192.168.1.3"),
-		netip.MustParseAddr("192.168.1.4"),
-		netip.MustParseAddr("192.168.1.7"),
-		netip.MustParseAddr("192.168.1.5"),
+	want := map[string]uint64{
+		"192.0.2.6":     8,
+		"192.0.2.3":     5,
+		"192.0.2.4":     4,
+		"192.0.2.7":     3,
+		"192.0.2.5":     2,
+		"192.0.100.100": 0,
 	}
 
-	assert.Equal(t, want, topk.Rank())
+	get := topk.Get()
+	//fmt.Println(topk.Get())
+	for k, v := range want {
+		assert.Equal(t, v, get[netip.MustParseAddr(k)])
+	}
+
+	wantRank := []netip.Addr{
+		netip.MustParseAddr("192.0.2.6"),
+		netip.MustParseAddr("192.0.2.3"),
+		netip.MustParseAddr("192.0.2.4"),
+		netip.MustParseAddr("192.0.2.7"),
+		netip.MustParseAddr("192.0.2.5"),
+	}
+
+	//fmt.Println(topk.Rank())
+	assert.Equal(t, wantRank, topk.Rank())
 }
+
+// TODO: nondeterministic test with:
+// * a large number of entries
+// * relatively small width*depth
+// * make sure we are within error bounds
 
 func BenchmarkAdd(b *testing.B) {
 	rand.Seed(time.Now().UnixNano())
@@ -69,7 +84,7 @@ func BenchmarkAdd(b *testing.B) {
 
 	for n := 0; n < b.N; n++ {
 		ip := func() string {
-			return fmt.Sprintf("192.168.%v.%v", rand.Intn(10-0)+0, rand.Intn(255-0)+0)
+			return fmt.Sprintf("192.0.%v.%v", rand.Intn(10-0)+0, rand.Intn(255-0)+0)
 		}
 		topk.Add(netip.MustParseAddr(ip()))
 		topk.Add(netip.MustParseAddr(ip()))
@@ -90,7 +105,7 @@ func BenchmarkGet(b *testing.B) {
 	topk := New(100, 100, 100, 0.99, 1234567812345678)
 
 	for n := 0; n < b.N; n++ {
-		ip := netip.MustParseAddr(fmt.Sprintf("192.168.1.%v", rand.Intn(255-0)+0))
+		ip := netip.MustParseAddr(fmt.Sprintf("192.0.2.%v", rand.Intn(255-0)+0))
 		topk.Add(ip)
 		topk.Get()
 	}
